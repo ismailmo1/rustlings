32fae81059757529aefe49de2318fa7662871ccb
ismailmo1 <ismail@sanogenetics.com>
2025-01-05 20:41:55 +0000

    chap 13

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -10,7 +10,13 @@ struct PositiveNonzeroInteger(u64);
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
         // TODO: This function shouldn't always return an `Ok`.
-        Ok(Self(value as u64))
+        if value.is_positive() {
+            Ok(Self(value as u64))
+        } else if value.is_negative() {
+            Err(CreationError::Negative)
+        } else {
+            Err(CreationError::Zero)
+        }
     }
 }
 
